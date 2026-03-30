5dbac6b02b8d1d51c14d1e76cee05f1a2f3761c5
M0stafa Yasser <141108885+mostafa3233@users.noreply.github.com>
2026-03-31 01:09:17 +0200

    creating entities Patient ,Post and PostReply

---
+++ Elaaj.Domain/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elaaj.Domain.Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public int PatientId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public ICollection<PostReply> postReplies { get; set; } = new List<PostReply>();
+
+}
---
+++ Elaaj.Domain/Entities/PostReply.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elaaj.Domain.Entities;
+
+public class PostReply
+{
+    public int Id { get; set; }
+    public int PatientId { get; set; }
+    public int PharmacyId { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int PostId { get; set; }
+
+}
