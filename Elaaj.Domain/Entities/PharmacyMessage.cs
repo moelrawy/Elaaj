ec01de67b782d25c0c756bf4703266ce9af333ef
moelrawy <mo.omar.elrawy@gmail.com>
2026-03-31 00:46:29 +0200

    Edit at pharmacy and create pharmacyMessage Entity

---
+++ Elaaj.Domain/Entities/PharmacyMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elaaj.Domain.Entities
+{
+    public class PharmacyMessage
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+
+
+        public int SenderPharmacyId { get; set; }
+        public Pharmacy SenderPharmacy { get; set; } = null!;
+
+        public int? ReciverPharmacyId { get; set; }
+        public Pharmacy? ReciverPharmacy { get; set; }
+    }
+}
