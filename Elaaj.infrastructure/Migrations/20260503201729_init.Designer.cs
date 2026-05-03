9c932e39810c7961486cc6395917ab347e50692d
moelrawy <mo.omar.elrawy@gmail.com>
2026-05-03 23:26:59 +0300

    Editing on program.cs (SignalR)

--- Elaaj.infrastructure/Migrations/20260503201729_init.Designer.cs
+++ Elaaj.infrastructure/Migrations/20260503201729_init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Elaaj.infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260503120911_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20260503201729_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
