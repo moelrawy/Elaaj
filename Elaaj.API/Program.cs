57de40b2e97664575c9bd439ffa580e961b59334
moelrawy <mo.omar.elrawy@gmail.com>
2026-05-03 17:26:10 +0300

    Revert "Merge pull request #21 from moelrawy/SignalR"
    This reverts commit 2f2a2f3a8a1a2bc1bd64d7bcf15d0dcec7349130, reversing
    changes made to 85b72f655b0b683f83aa59da287b70d2d58759d1.

--- Elaaj.API/Program.cs
+++ Elaaj.API/Program.cs
@@ -4,45 +4,23 @@ using Elaaj.Application.Extensions;
 using Elaaj.Domain.Entities;
 using Elaaj.infrastructure.Seeders;
 using Elaaj.Infrastructure.Extensions;
-using Elaaj.API.Hubs;
-using Elaaj.API.Services;
-using Elaaj.Application.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-//Add SignalR services
-builder.Services.AddSignalR();
+
 // Add services to the container.
 
 
 builder.AddPresentation();
-
-// MediatR
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials()
-              .SetIsOriginAllowed(host => true);
-    });
-});
-
-
 
 var app = builder.Build();
+
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
-//CORS
-app.UseCors("AllowAll");
-
 
 await seeder.Seed();
 
@@ -65,7 +43,5 @@ app.MapGroup("api/identity")
 app.UseAuthorization();
 
 app.MapControllers();
-builder.Services.AddScoped<INotificationService, NotificationService>();
-app.MapHub<NotificationHub>("/notificationsHub");
 
 app.Run();
