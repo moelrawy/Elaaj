5723fdceee6ecdd734e428d3eafea36f58f00fed
moelrawy <mo.omar.elrawy@gmail.com>
2026-05-08 16:08:20 +0300

    ConvertingUserIdAcceptionFromTokenNotFromRequest-Authorization
    ConvertingUserIdAcceptionFromTokenNotFromRequest
    Authorization: Edit at code

--- Elaaj.API/Controllers/PostRepliesController.cs
+++ Elaaj.API/Controllers/PostRepliesController.cs
@@ -1,10 +1,13 @@
-﻿using MediatR;
+﻿using Elaaj.Application.Features.PostReplies.Commands.CreatePostReply;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Elaaj.Application.Features.PostReplies.Commands.CreatePostReply;
+using Restaurants.Domain.Constants;
 namespace Elaaj.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class PostRepliesController:ControllerBase
     {
         private readonly ISender _mediator;
@@ -12,7 +15,9 @@ namespace Elaaj.API.Controllers
         {
             _mediator = mediator;
         }
+
         [HttpPost]
+        [Authorize(Roles = $"{UserRoles.PharmacyOwner},{UserRoles.PharmacyAdmin}")] //  Only pharmacy staff
         public async Task<IActionResult> CreateReply([FromBody] CreatePostReplyCommand command)
         {
             var result = await _mediator.Send(command);
