bdb561172c0243c4d6ea6f52731f7b890366bc1d
moelrawy <mo.omar.elrawy@gmail.com>
2026-05-22 11:34:48 +0300

    publish settings

--- Elaaj.API/Program.cs
+++ Elaaj.API/Program.cs
@@ -53,14 +53,11 @@ await seeder.Seed();
 //app.UseMiddleware<ErrorHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elaaj API v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elaaj API v1");
+});
 
 app.UseHttpsRedirection();
 
